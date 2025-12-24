aebfcca98c3199e58e452d6ef49eabeca06497d1
Paul Northrop <p.northrop@ucl.ac.uk>
2025-12-24 15:02:01 +0000

    Add tests

--- tests/testthat/test-gev_return.R
+++ tests/testthat/test-gev_return.R
@@ -25,9 +25,23 @@ test_that("RL CI upper limits increase with return period, faster = FALSE", {
 # Plot method
 interval1 <- plot(prof, parm = 1)
 interval2 <- plot(prof, parm = 2)
-test_that("Symmetric intervals for Poisson GLM", {
+test_that("Symmetric intervals for return level 1", {
   expect_equal(interval1, prof[1, ])
 })
-test_that("Symmetric intervals for Poisson GLM", {
+test_that("Symmetric intervals for return level 2", {
   expect_equal(interval2, prof[2, ])
+})
+
+# Repeat for gev_weighted()
+
+fit <- gev_weighted(PlymouthOzoneMaxima)
+rl <- gev_return(fit, m = c(50, 100))
+
+# Profile-based confidence intervals, faster = TRUE
+prof <- confint(rl, profile = TRUE, mult = 32, faster = TRUE)
+test_that("RL CI lower limits increase with return period, faster = TRUE", {
+  testthat::expect_lt(prof[1, 1], prof[2, 1])
+})
+test_that("RL CI upper limits increase with return period, faster = TRUE", {
+  testthat::expect_lt(prof[1, 2], prof[2, 2])
 })
