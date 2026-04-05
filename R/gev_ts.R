69aaa9a1c409a8d48bf7ed8e1d717a75b3eb0286
paulnorthrop <p.northrop@ucl.ac.uk>
2026-04-05 21:20:03 +0100

    Change of class names

--- R/gev_ts.R
+++ R/gev_ts.R
@@ -142,12 +142,11 @@ gev_ts <- function(data, block_length, block, pseudo = TRUE, sliding = FALSE,
   # Otherwise, use block_maxima_ts() to calculate the block maxima, the numbers
   # of non-missing values in the blocks and the largest possible number of
   # non-missing values in each block.
-  from_maxima_ts <- inherits(data, "block_maxima") ||
-    inherits(data, "sliding_block_maxima")
+  from_maxima <- inherits(data, "block_maxima") || inherits(data, "sliding")
   required <- c("maxima", "notNA", "n", "whereNA", "pseudo_maxima",
                 "full_maxima", "partial_maxima")
   has_components <- all(is.element(required, names(data)))
-  if (from_maxima_ts && has_components && inherits(data, "evmissing")) {
+  if (from_maxima && has_components && inherits(data, "evmissing")) {
     maxima_notNA <- data
   } else if (is.list(data)) {
     if (has_components) {
